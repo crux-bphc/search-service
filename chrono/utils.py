1005de7a5285caf6869c93be3b6dd50b471723d6
Chaitanya-Keyal <66475772+Chaitanya-Keyal@users.noreply.github.com>
2024-07-16 04:35:01 +0530

    feat: add func to remove \n chars

--- chrono/utils.py
+++ chrono/utils.py
@@ -9,11 +9,24 @@ def insert_courses():
     for course in courses:
         course_id = course["id"]
         print(course_id)
-        course_with_sections = requests.get(
+        course = requests.get(
             f"https://www.chrono.crux-bphc.com/api/course/{course_id}"
         ).json()
-        course_inserted = client.index(index=COURSE_INDEX, body=course_with_sections)
+        course = remove_newline_chars(course)
+        course_inserted = client.index(index=COURSE_INDEX, body=course)
         pprint(course_inserted.body)
+
+
+def remove_newline_chars(object):
+    if isinstance(object, dict):
+        for key, value in object.items():
+            object[key] = remove_newline_chars(value)
+    elif isinstance(object, list):
+        for index, value in enumerate(object):
+            object[index] = remove_newline_chars(value)
+    elif isinstance(object, str):
+        object = object.replace("\n", " ")
+    return object
 
 
 if __name__ == "__main__":
