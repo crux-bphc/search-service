9e11a87c7f84728fb113b457baa2f743ac0f3594
Chaitanya-Keyal <66475772+Chaitanya-Keyal@users.noreply.github.com>
2024-07-16 04:37:08 +0530

    feat: add courses data when timetable is added

--- chrono/timetable.py
+++ chrono/timetable.py
@@ -1,4 +1,6 @@
+from elasticsearch_setup import COURSE_INDEX, client
 from flask import Blueprint, jsonify, request
+from utils import remove_newline_chars
 
 timetable = Blueprint("timetable", __name__)
 
@@ -12,6 +14,24 @@ def search_timetable():
 @timetable.route("/add", methods=["POST"])
 def add_timetable():
     timetable_data = request.json
+
+    # Add course information to timetable_data
+    timetable_data["courses"] = []
+    course_ids = {section["courseId"] for section in timetable_data["sections"]}
+    for course_id in course_ids:
+        course = client.search(
+            index=COURSE_INDEX,
+            body={"query": {"term": {"id": course_id}}},
+            size=1,
+        )["hits"]["hits"][0]["_source"]
+        timetable_data["courses"].append(
+            {
+                "code": course["code"],
+                "name": course["name"],
+            }
+        )
+
+    timetable_data = remove_newline_chars(timetable_data)
     return jsonify(timetable_data), 201
 
 
