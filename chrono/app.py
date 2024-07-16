789fd9ac219bf87345da6d2f21dd5b069743595a
skoriop <karthikp.ivy@gmail.com>
2024-07-16 21:28:33 +0530

    fix: bind flask backend to all interfaces

--- chrono/app.py
+++ chrono/app.py
@@ -13,4 +13,4 @@ app.register_blueprint(course, url_prefix="/course")
 app.register_blueprint(timetable, url_prefix="/timetable")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv("CHRONO_PORT"))
+    app.run(host="0.0.0.0", port=os.getenv("CHRONO_PORT"), debug=True)
