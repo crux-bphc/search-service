237175b3a88bf7006d6c196ae2b9bef97d88e046
Chaitanya-Keyal <66475772+Chaitanya-Keyal@users.noreply.github.com>
2024-07-16 06:51:49 +0530

    fix: update course code mapping

--- chrono/elasticsearch_setup.py
+++ chrono/elasticsearch_setup.py
@@ -23,7 +23,7 @@ def create_course_index():
         "mappings": {
             "properties": {
                 "id": {"type": "keyword"},
-                "code": {"type": "keyword"},
+                "code": {"type": "search_as_you_type"},
                 "name": {"type": "search_as_you_type"},
                 "sections": {
                     "type": "nested",
@@ -89,7 +89,7 @@ def create_timetable_index():
                 "courses": {  # Added by search service when a timetable is added
                     "type": "nested",
                     "properties": {
-                        "code": {"type": "keyword"},
+                        "code": {"type": "search_as_you_type"},
                         "name": {"type": "search_as_you_type"},
                     },
                 },
